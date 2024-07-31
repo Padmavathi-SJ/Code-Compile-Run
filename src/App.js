02715c9e6aab0c91395aa110ad0de1f80ce6f580
PADMAVATHI26 <padmasj54@gmail.com>
2024-08-01 00:45:04 +0530

    Navigation Changed

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
+// src/App.js
 import React from 'react';
+import MainPage from './Components/MainPage';
 import './App.css';
-import Home from './Components/Home';
 
 const App = () => {
-  return (
-  <div>
-    <Home />
-
-  </div>
-  )
-}
-
-export default App;
-
-
-
-/* import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import LoginPage from './Components/LoginPage';
-import Home from './Components/Home';
-
-function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={
-          isAuthenticated ? <Navigate to="/" /> : <LoginPage onLogin={handleLogin} />}
-      />
-        <Route path="/" element={
-          isAuthenticated ? <Home /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </Router>
-    <div>
-      <Home />
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <MainPage />
+        </div>
+    );
+};
 
 export default App;
-*/
